df27cf07a6261eeff1c44db630b76e43134b064f
Itzan Casales Gutiérrez <itzan.casales@gmail.com>
2026-09-16 10:08:51 +0200

    estilos base (paleta + tipografia)

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,7 +2,7 @@ export default defineNuxtConfig({
   // Get all the pages, components, composables and plugins from the parent theme
   extends: ['./woonuxt_base'],
 
-  css: ['~/app/assets/css/custom.css'],
+  css: ['~/assets/css/custom.css'],
 
   components: [{ path: './components', pathPrefix: false }],
 
