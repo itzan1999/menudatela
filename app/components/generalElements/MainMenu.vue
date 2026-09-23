f528419ced7905b1bfc1348a0f1dc9de26ffd944
Itzan Casales Gutiérrez <itzan.casales@gmail.com>
2026-09-23 10:00:37 +0200

    añadida animacion al menu del header

--- app/components/generalElements/MainMenu.vue
+++ app/components/generalElements/MainMenu.vue
@@ -23,10 +23,40 @@ const route = useRoute();
   color: var(--color-charcoal);
   opacity: 0.7;
   transition: opacity 0.2s ease;
+  position: relative;
+  text-decoration: none;
+  padding: 0.3rem 0;
+  margin: 0 1rem;
 }
 
-.nav-link:hover,
+.nav-link::after {
+  content: '';
+  position: absolute;
+  left: 0;
+  bottom: 0;
+  width: 100%;
+  height: 2px;
+  opacity: 0.7;
+  background: var(--color-charcoal);
+  transform: scaleX(0);
+  transform-origin: center;
+  transition: transform 0.3s ease;
+}
+
+.nav-link:hover::after,
+.nav-link.router-link-active::after {
+  transform: scaleX(1);
+}
+
+/* .nav-link:hover,
 .nav-link.router-link-active {
   opacity: 1;
+} */
+
+.nav-link.router-link-active,
+.nav-link.router-link-active::after {
+  opacity: 1;
+  color: var(--color-charcoal);
+  font-weight: bold;
 }
 </style>
